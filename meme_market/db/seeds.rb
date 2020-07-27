8496857be8db7c3bb1a2092f79e2f9376d6698d7
Anfii <Anthonywilde117@gmail.com>
2020-07-27 12:28:06 +1000

    Finished work on Devise User Model seeded DB with 5 examples of a User Object

--- meme_market/db/seeds.rb
+++ meme_market/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    User.create(username: "Carl", email:"Carlsan@daijobu.com", password: "123456")
+    User.create(username: "Zeb", email: "Zeb@Zeb.com", password: "turtleneckndchain")
+    User.create(username: "Anfii", email: "Anfii@Anfii.com", password: "Therealanfii")
+    User.create(username: "Dane", email: "Myfrienddane@Dane.com", password: "hadaneha")
+    User.create(username: "Latchy", email: "Eggboi@eggboi.com", password: "heliketheeggs")
+ 
