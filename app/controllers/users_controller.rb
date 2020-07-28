4323dc54721cb49b758ed358ca7d115d40035449
Anfii <Anthonywilde117@gmail.com>
2020-07-28 17:14:42 +1000

    final push for day

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+    #before_action :authenticate_user!
 
+    #Login page
     
+    def index
+        @users = User.all
+    end
+    #New User Page
+    def new
+    @user = User.new
+    end
+    #Create User Page
+    def create
+    @user = User.new(user_params)
+    @user.save
+    end
+    #Show User Page
+
+    #Edit User Page
+
+    #Delete User Page?
+
+
+    private
+    def user_params
+        require(:user).permit(:username, :email, :password)
+    end
 end
