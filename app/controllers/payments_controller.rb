6300020df0717aea8c196130a61726fcae9a4275
Anfii <Anthonywilde117@gmail.com>
2020-08-10 12:21:42 +1000

    webhook update

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,17 +8,14 @@ class PaymentsController < ApplicationController
     end
 
     def webhook
-        puts "transferring ownership"
-        pp params
-        puts "-----------------------"
-        Meme.find(params[:meme_id]).update(user_id: current_user) 
-        puts "ownership transfered to #{current_user.username}"
-        puts "-----------------------"
-
+        
+         
+        
         payment_id = params[:data][:object][:payment_intent]
         payment = Stripe::PaymentIntent.retieve(payment_id)
         meme_id = payment.metadata.meme_id
         user_id = payment.metadata.user_id
+        Meme.find(meme_id).update(user_id: user_id)
 
         p "meme id" + meme_id
         p "user_id" + user_id
